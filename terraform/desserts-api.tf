ccc81980dcb1f6d7bbdf77f99e87b8105a8d7275
Anthony Viera <anthonyviera4@gmail.com>
2025-05-20 11:10:11 -0500

    again

--- terraform/desserts-api.tf
+++ terraform/desserts-api.tf
@@ -140,6 +140,7 @@ resource "aws_iam_policy" "desserts_api_tf_deployer_policy" {
           "s3:GetBucketWebsite",
           "s3:GetBucketVersioning",
           "s3:GetAccelerateConfiguration",
+          "s3:GetBucketRequestPayment",
         ],
         Resource = [
           data.aws_s3_bucket.dessert_images_bucket.arn
