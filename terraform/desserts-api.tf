74c1bee91d9b2f122f6c0abd9f6f6153cce36288
Anthony Viera <anthonyviera4@gmail.com>
2025-05-20 22:25:37 -0500

    ecr perm update

--- terraform/desserts-api.tf
+++ terraform/desserts-api.tf
@@ -108,11 +108,17 @@ resource "aws_iam_policy" "desserts_api_tf_deployer_policy" {
           "ecr:DescribeRepositories",
           "ecr:DescribeImages",
           "ecr:ListTagsForResource",
-          "ecr:GetAuthorizationToken",
         ],
         Resource = [
           data.aws_ecr_repository.desserts_api_repository.arn
         ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken",
+        ],
+        Resource = ["*"]
       },
       {
         Effect = "Allow",
