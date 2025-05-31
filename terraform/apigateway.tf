f6974c272afbb8cfacf1b4a5882dcb3b1a466f9b
Anthony Viera <anthonyviera4@gmail.com>
2025-05-30 20:32:55 -0500

    just for now

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -6,10 +6,10 @@ data "aws_api_gateway_domain_name" "desserts_domain_name" {
   domain_name = var.environment == "prod" ? "desserts-api.megsparadisecakes.com" : "desserts-dev-api.megsparadisecakes.com"
 }
 
-data "aws_api_gateway_rest_api" "orders_rest_api" {
-  name = "orders-api-gateway"
-}
+# data "aws_api_gateway_rest_api" "orders_rest_api" {
+#   name = "orders-api-gateway"
+# }
 
-data "aws_api_gateway_domain_name" "orders_domain_name" {
-  domain_name = var.environment == "prod" ? "orders-api.megsparadisecakes.com" : "orders-dev-api.megsparadisecakes.com"
-}
+# data "aws_api_gateway_domain_name" "orders_domain_name" {
+#   domain_name = var.environment == "prod" ? "orders-api.megsparadisecakes.com" : "orders-dev-api.megsparadisecakes.com"
+# }
